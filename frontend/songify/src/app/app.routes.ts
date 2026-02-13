3ec0271118dfef129a3cea5c6d92ab4dc7505e24
delima-justine <delimajustine24@gmail.com>
2026-02-13 23:30:26 +0800

    refactor: remove current frontend setup.

--- frontend/songify/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./landing-page/landing-page')
-      .then(m => m.LandingPage)
-  },
-];
