f284c1587492241337b006a1cf7d771e197e366f
delima-justine <delimajustine24@gmail.com>
2026-02-15 13:39:17 +0800

    feat: add route for main page component

--- frontend/songify/src/app/app.routes.ts
+++ frontend/songify/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./landing-page/landing-page').then(m => m.LandingPage)
+  },
+  {
+    path: 'main-page',
+    loadComponent: () => import('./main-page/main-page').then(m => m.MainPage)
   }
 ];
